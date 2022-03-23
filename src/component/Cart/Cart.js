646f851190c99e53a6176088fbf2ce1ecd875b1f
sbsohag3e <sbsohag3@gmaie.com>
2022-03-23 13:07:12 +0600

    added cart button section

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,3 +1,6 @@
+import { faArrowRight, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './Cart.css'
 
@@ -18,12 +21,24 @@ const Cart = ({cart}) => {
     const grandTotal = total + shipping + tax;
     return (
         <div className='cart'>
+            <div>
             <h3>Order Summary</h3>
-              <p>Selected Items : {cart.length}</p>
+              <p>Selected Items : {quantity}</p>
               <p>Total Price : ${total}</p>
               <p>Total Shipping : ${shipping}</p>
               <p>Tax : ${tax}</p>
-              <h5>Grand Total : ${grandTotal.toFixed(2)}</h5>
+              <h5  className='grandtotal'>Grand Total : ${grandTotal.toFixed(2)}</h5>
+            </div>
+            <div>
+                <button className='cart-btn'>
+                    <p className='clear-carttext'>Clear Cart</p>
+                    <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>
+                </button><br />
+                <button className='review-order'>
+                    <p className='review-ordertext'>Review Order</p>
+                    <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+                    </button>
+            </div>
         </div>
     );
 };
